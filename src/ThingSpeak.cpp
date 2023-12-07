b8927bbe3e1999ed042352f458b75d5fd51d56e7
klr003 <99308711+klr003@users.noreply.github.com>
2023-12-07 14:05:17 -0600

    Initial commit

---
+++ src/ThingSpeak.cpp
@@ -0,0 +1,29 @@
+#include "Grove-Ultrasonic-Ranger.h"
+
+#include "ThingSpeak.h"
+
+TCPClient client;
+Ultrasonic ultrasonic(4);
+
+unsigned long myChannelNumber = 2340176;    // change this to your channel number
+const char * myWriteAPIKey = "948IKJBI13627EZE"; // change this to your channels write API key
+
+void setup() {
+    ThingSpeak.begin(client);
+    Serial.begin(9600);
+}
+
+void loop() {
+
+ long RangeInCentimeters;
+
+ Serial.println("The distance to obstacles in front is: ");
+ 
+ RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+ Serial.print(RangeInCentimeters);//0~400cm
+ Serial.println(" cm");
+ 
+ ThingSpeak.writeField(myChannelNumber, 1, RangeInCentimeters, myWriteAPIKey);
+ delay(20000); // ThingSpeak will only accept updates every 15 seconds.
+
+}
